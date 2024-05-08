7f86776cb944c5e453b322fea14f7bc9fb8228fe
kenkipkoec <itsmetrokenaki@gmal.com>
2024-05-08 15:08:29 +0300

    rendered the about.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-
+import React from "react";
+import AboutUs from "./AboutUs"; 
 
 function App() {
- 
-
   return (
     <>
-      
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to Cool Blog</h1>
+        </header>
+        <main>
+          <AboutUs />
+        </main>
+        <footer>
+          
+        </footer>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
