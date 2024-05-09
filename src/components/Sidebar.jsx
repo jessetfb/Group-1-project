608c26b7d9555be443bc3ae63a7949c3209ab865
lizwachira <lizwanguwachira@gmail.com>
2024-05-09 06:41:02 +0300

    feat: added socialmedia icons https://react-icons.github.io/

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
 import "./sidebar.css";
 
 function Sidebar() {
@@ -25,12 +26,47 @@ function Sidebar() {
       </div>
       <div className="sidebarItem">
         <span className="sidebarTitle">FOLLOW US</span>
-        <div className="sidebarSocial">
-        <i className="sidebarIcon fab fa-facebook-square"></i>
-        <i className="sidebarIcon fab fa-twitter-square"></i>
-        <i className="sidebarIcon fab fa-pinterest-square"></i>
-        <i className="sidebarIcon fab fa-instagram-square"></i>
-        </div>
+        <footer className="bg-gradient-to-r from-blue-500 to-purple-500 text-white py-12">
+<div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+<div className="mb-6 md:mb-0">
+<h3 className="text-2xl font-bold mb-4">Stay Connected</h3>
+<p className="text-lg">Follow us on social media:</p>
+<div className="flex space-x-4 mt-4">
+<a href="https://web.facebook.com/" className="text-white hover:text-gray-300">
+<FaFacebook size={24} />
+</a>
+<a href="https://twitter.com/" className="text-white hover:text-gray-300">
+<FaTwitter size={24} />
+</a>
+<a href="https://www.instagram.com/" className="text-white hover:text-gray-300">
+<FaInstagram size={24} />
+</a>
+<a href="https://www.linkedin.com/" className="text-white hover:text-gray-300">
+<FaLinkedin size={24} />
+</a>
+<a href="https://www.linkedin.com/" className="text-white hover:text-gray-300">
+<FaGithub size={24} />
+</a>
+</div>
+</div>
+<div>
+<h3 className="text-2xl font-bold mb-4">Subscribe to our newsletter</h3>
+<form className="flex items-center">
+<input
+type="email"
+placeholder="Enter your email"
+className="bg-white text-gray-800 px-4 py-2 rounded-l-md focus:outline-none"
+/>
+<button
+type="submit"
+className="bg-gray-800 text-white px-4 py-2 rounded-r-md hover:bg-gray-700 focus:outline-none"
+>
+Subscribe
+</button>
+</form>
+</div>
+</div>
+</footer>
       </div>
     </div>
   );
