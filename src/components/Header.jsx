a0e39ed4fb121495c6337bcfa70f561618693da0
Denis <mugambimuthuri0@gmail.com>
2024-05-09 06:21:21 +0300

    commented Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
-import "./header.css";
-import React from "react";
+import "./header.css"; // Importing the CSS file for styling
+import React from "react"; // Importing React library
 
-function Header() {
-  return (
-    <div className="header">
-      <div className="headerTitles">
-        <span className="headerTitleSm">Cool</span>
-        <span className="headerTitleLg">Blog</span>
+function Header() { // Defining the Header component
+  return ( // Returning JSX elements
+    <div className="header"> {/* Container for header */}
+      <div className="headerTitles"> {/* Container for header titles */}
+        <span className="headerTitleSm">Cool</span> {/* Small header title */}
+        <span className="headerTitleLg">Blog</span> {/* Large header title */}
       </div>
-      <img className="headerImg" src="https://images.pexels.com/photos/163255/sunrise-sun-morgenrot-skies-163255.jpeg?cs=srgb&dl=pexels-pixabay-163255.jpg&fm=jpg" alt="" />
+      <img className="headerImg" src="https://images.pexels.com/photos/163255/sunrise-sun-morgenrot-skies-163255.jpeg?cs=srgb&dl=pexels-pixabay-163255.jpg&fm=jpg" alt="" /> {/* Image for header */}
     </div>
   );
 }
-export default Header;
+export default Header; // Exporting the Header component
