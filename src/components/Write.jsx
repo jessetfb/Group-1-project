66d9bb816b8bd168d7a31867c252b6a9aeeb21cd
Denis <mugambimuthuri0@gmail.com>
2024-05-12 12:28:21 +0300

    Made the write submit button work

--- src/components/Write.jsx
+++ src/components/Write.jsx
@@ -1,7 +1,18 @@
 import "./write.css"; // Importing the CSS file for styling
-import React from "react"; // Importing React library
+import React, { useState } from "react"; // Importing React library
 
 function Write() { // Defining the Write component
+  const [title, setTitle] = useState(""); // State for title input
+  const [content, setContent] = useState(""); // State for content input
+
+  // Function to handle form submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Perform actions like sending data to the server or updating state
+    console.log("Title:", title);
+    console.log("Content:", content);
+  };
+
   return ( // Returning JSX elements
     <div className="write"> {/* Container for writing */}
       <img
@@ -9,17 +20,19 @@ function Write() { // Defining the Write component
         src="https://c4.wallpaperflare.com/wallpaper/361/37/200/zebras-in-amboseli-national-park-mount-kilimanjaro-in-southern-kenya-4k-ultra-hd-desktop-wallpapers-for-computers-laptop-tablet-and-mobile-phones-3840%C3%972160-wallpaper-preview.jpg"
         alt=""
       /> {/* Image for writing */}
-      <form className="writeForm"> {/* Form for writing */}
+      <form className="writeForm" onSubmit={handleSubmit}> {/* Form for writing */}
         <div className="writeFormGroup"> {/* Form group for input */}
           <label htmlFor="fileInput"> {/* Label for file input */}
             <i className="writeIcon fa-solid fa-plus"></i> {/* Icon for adding files */}
           </label>
           <input type="file" id="fileInput" style={{ display: "none" }} /> {/* File input for adding files */}
           <input
-            type="Text"
+            type="text"
             placeholder="Title"
             className="writeInput"
             autoFocus={true}
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
           /> {/* Input field for title */}
         </div>
         <div className="writeFormGroup"> {/* Form group for text area */}
@@ -27,9 +40,11 @@ function Write() { // Defining the Write component
             placeholder="Tell your Story"
             type="text"
             className="writeInput writeText"
+            value={content}
+            onChange={(event) => setContent(event.target.value)}
           ></textarea> {/* Text area for writing */}
         </div>
-        <button className="writeSubmit">Publish</button> {/* Publish button */}
+        <button type="submit" className="writeSubmit">Publish</button> {/* Publish button */}
       </form>
     </div>
   );
